80be1f078f8f64f253d5788466045bdf71735fc5
Jung-Yi, Tsai <jungyitsai@gmail.com>
2022-07-16 04:08:29 +0800

    [add] word p, r, f1 evaluation

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 import numpy
 import string
 import argparse
+import os
 
 import torch
 import torch.backends.cudnn as cudnn
 import torch.utils.data
 import torch.nn.functional as F
+import pandas as pd
 
 from utils import CTCLabelConverter, AttnLabelConverter, TransformerLabelConverter
 from dataset import RawDataset, AlignCollate
@@ -93,10 +95,108 @@ def demo(opt):
                 log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
 
             log.close()
+            
+
+def output_word_acc(opt):
+    """
+    """
+    if 'CTC' in opt.Prediction:
+        converter = CTCLabelConverter(opt.character)
+    elif "Transformer" in opt.Prediction:
+        converter = TransformerLabelConverter(opt.character)
+    else:
+        converter = AttnLabelConverter(opt.character)
+    opt.num_class = len(converter.character)
+
+    if opt.rgb:
+        opt.input_channel = 3
+    model = Model(opt)
+    print('model input parameters', opt.imgH, opt.imgW, opt.num_fiducial, opt.input_channel, opt.output_channel,
+          opt.hidden_size, opt.num_class, opt.batch_max_length, opt.Transformation, opt.FeatureExtraction,
+          opt.SequenceModeling, opt.Prediction)
+    model = torch.nn.DataParallel(model).to(device)
+
+    # load model
+    print('loading pretrained model from %s' % opt.saved_model)
+    model.load_state_dict(torch.load(opt.saved_model, map_location=device))
+
+    # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
+    AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
+    demo_data = RawDataset(root=opt.image_folder, opt=opt)  # use RawDataset
+    demo_loader = torch.utils.data.DataLoader(
+        demo_data, batch_size=opt.batch_size,
+        shuffle=False,
+        num_workers=int(opt.workers),
+        collate_fn=AlignCollate_demo, pin_memory=True)
+
+    # predict
+    model.eval()
+    with torch.no_grad():
+        pred_df = pd.DataFrame(data=[], columns = ['image_path', 'preds_str', 'preds_max_prob'])
+        for image_tensors, image_path_list in demo_loader:
+            batch_size = image_tensors.size(0)
+            image = image_tensors.to(device)
+            # For max length prediction
+            length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(device)
+            text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
+
+            if 'CTC' in opt.Prediction:
+                preds = model(image, text_for_pred)
+
+                # Select max probabilty (greedy decoding) then decode index to character
+                preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+                _, preds_index = preds.max(2)
+                # preds_index = preds_index.view(-1)
+                preds_str = converter.decode(preds_index, preds_size)
+
+            else:
+                preds = model(image, text_for_pred, is_train=False)
+
+                # select max probabilty (greedy decoding) then decode index to character
+                _, preds_index = preds.max(2)
+                preds_str = converter.decode(preds_index, length_for_pred)
+
+
+            log = open(f'./log_demo_result.txt', 'a')
+            dashed_line = '-' * 80
+            head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'
+            
+            print(f'{dashed_line}\n{head}\n{dashed_line}')
+            log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
+
+            preds_prob = F.softmax(preds, dim=2)
+            preds_max_prob, _ = preds_prob.max(dim=2)
+            for img_name, pred, pred_max_prob in zip(image_path_list, preds_str, preds_max_prob):
+                if 'Attn' in opt.Prediction or "Transformer" in opt.Prediction:
+                    pred_EOS = pred.find('[s]')
+                    pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                    pred_max_prob = pred_max_prob[:pred_EOS]
+
+                # calculate confidence score (= multiply of pred_max_prob)
+                try:
+                    confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+                except IndexError:
+                    confidence_score = 0.0
+
+                print(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}')
+                log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
+
+            log.close()
+            
+            # output dataframe result
+            list_of_preds = list(zip(image_path_list, preds_str, preds_max_prob))
+            curr_pred_df = pd.DataFrame(list_of_preds, columns = ['image_path', 'preds_str', 'preds_max_prob'])
+            pred_df = pred_df.append(curr_pred_df, ignore_index=True)
+            
+        pred_result_path = os.path.join(opt.output_path, 'pred.csv')
+        pred_df.to_csv(pred_result_path, encoding='utf-8', sep='\t', index=False)
+            
+            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image_folder', required=True, help='path to image_folder which contains text images')
+    parser.add_argument('--output_path', required=True, help='path to predict result csv')
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
     parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
     parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
@@ -129,4 +229,5 @@ if __name__ == '__main__':
     cudnn.deterministic = True
     opt.num_gpu = torch.cuda.device_count()
 
-    demo(opt)
+    # demo(opt)
+    output_word_acc(opt)
